d1db914f611402e32f370f41d6075e37ea76427e
ItsAndre548 <andre548luiz@gmail.com>
2024-10-10 00:18:57 -0300

    Fix:TopBar margin in mobile

--- sections/TopBar.tsx
+++ sections/TopBar.tsx
@@ -8,7 +8,7 @@ export interface TopItem {
    * @title Imagem
    * @description Tamanho da imagem (largura: 50px, altura: 50px) */
   image: ImageWidget;
-   /**
+  /**
    * @title Imagem Mobile
    * @description Tamanho da imagem (largura: 50px, altura: 50px) */
   mobileImage: ImageWidget;
@@ -34,35 +34,35 @@ interface Props {
 }
 
 export default function TopBar({ content }: Props) {
-  const device = useDevice()
-  if(device !== "desktop") {
-    return(
-      <div class="flex items-center overflow-auto mx-auto mt-5 py-auto bg-primary w-screen md:max-w-[1440px] h-auto">
-      {content.map((item) => (
-        <a
-          href={item.href}
-          target="blank"
-          class="flex items-center w-full mr-7 md:mr-0 ml-6 h-[200px]"
-        >
-          <div class="flex justify-center items-center w-[50px] h-[50px]">
-            {item.mobileImage && (
-              <Image
-                class="lg:w-[94px] w-full lg:h-[50px] flex-shrink-0 object-contain"
-                src={item.mobileImage}
-                alt={item.title}
-                width={94}
-                height={34}
-              />
-            )}
-          </div>
-          <div class="ml-6 w-[200px] text-base text-primary-content">
-            <p class="mb-2 font-semibold text-xs">{item.title}</p>
-            <p class="font-light text-xs">{item.subtitle}</p>
-          </div>
-        </a>
-      ))}
-    </div>
-    )
+  const device = useDevice();
+  if (device !== "desktop") {
+    return (
+      <div class="flex items-center overflow-auto mx-auto md:mt-5 py-auto bg-primary w-screen md:max-w-[1440px] h-auto">
+        {content.map((item) => (
+          <a
+            href={item.href}
+            target="blank"
+            class="flex items-center w-full mr-7 md:mr-0 ml-6 h-[200px]"
+          >
+            <div class="flex justify-center items-center w-[50px] h-[50px]">
+              {item.mobileImage && (
+                <Image
+                  class="lg:w-[94px] w-full lg:h-[50px] flex-shrink-0 object-contain"
+                  src={item.mobileImage}
+                  alt={item.title}
+                  width={94}
+                  height={34}
+                />
+              )}
+            </div>
+            <div class="ml-6 w-[200px] text-base text-primary-content">
+              <p class="mb-2 font-semibold text-xs">{item.title}</p>
+              <p class="font-light text-xs">{item.subtitle}</p>
+            </div>
+          </a>
+        ))}
+      </div>
+    );
   }
   return (
     <div class="flex md:justify-around items-center mx-auto mt-5 py-auto bg-primary lg:bg-primary-content w-screen md:max-w-[1440px] h-auto md:overflow-hidden">
