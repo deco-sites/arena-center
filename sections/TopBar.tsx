9002400e0c109db89401d7e1d172874813d1addd
ItsAndre548 <andre548luiz@gmail.com>
2024-10-09 23:15:06 -0300

    TopBar

--- sections/TopBar.tsx
+++ sections/TopBar.tsx
@@ -1,12 +1,17 @@
 import { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
+import { useDevice } from "@deco/deco/hooks";
 
 /** @titleBy title */
 export interface TopItem {
   /**
    * @title Imagem
    * @description Tamanho da imagem (largura: 50px, altura: 50px) */
-  image?: ImageWidget;
+  image: ImageWidget;
+   /**
+   * @title Imagem Mobile
+   * @description Tamanho da imagem (largura: 50px, altura: 50px) */
+  mobileImage: ImageWidget;
   /**
    * @title Link
    */
@@ -29,8 +34,38 @@ interface Props {
 }
 
 export default function TopBar({ content }: Props) {
+  const device = useDevice()
+  if(device !== "desktop") {
+    return(
+      <div class="flex items-center overflow-auto mx-auto mt-5 py-auto bg-primary w-screen md:max-w-[1440px] h-auto">
+      {content.map((item) => (
+        <a
+          href={item.href}
+          target="blank"
+          class="flex items-center w-full mr-7 md:mr-0 ml-6 h-[200px]"
+        >
+          <div class="flex justify-center items-center w-[50px] h-[50px]">
+            {item.mobileImage && (
+              <Image
+                class="lg:w-[94px] w-full lg:h-[50px] flex-shrink-0 object-contain"
+                src={item.mobileImage}
+                alt={item.title}
+                width={94}
+                height={34}
+              />
+            )}
+          </div>
+          <div class="ml-6 w-[200px] text-base text-primary-content">
+            <p class="mb-2 font-semibold text-xs">{item.title}</p>
+            <p class="font-light text-xs">{item.subtitle}</p>
+          </div>
+        </a>
+      ))}
+    </div>
+    )
+  }
   return (
-    <div class="flex md:justify-between items-center mx-auto mt-5 py-auto w-screen md:max-w-[1400px] h-auto md:overflow-hidden">
+    <div class="flex md:justify-around items-center mx-auto mt-5 py-auto bg-primary lg:bg-primary-content w-screen md:max-w-[1440px] h-auto md:overflow-hidden">
       {content.map((item) => (
         <a
           href={item.href}
@@ -40,7 +75,7 @@ export default function TopBar({ content }: Props) {
           <div class="flex justify-center items-center w-[50px] h-[50px]">
             {item.image && (
               <Image
-                class="w-[94px] h-[50px] object-contain"
+                class="lg:w-[94px] w-full lg:h-[50px] object-contain text-primary-content lg:text-secondary"
                 src={item.image}
                 alt={item.title}
                 width={94}
@@ -48,7 +83,7 @@ export default function TopBar({ content }: Props) {
               />
             )}
           </div>
-          <div class="ml-6 w-[200px] text-base text-secondary">
+          <div class="ml-6 w-[200px] text-base text-primary-content lg:text-secondary">
             <p class="mb-2 font-semibold text-xs">{item.title}</p>
             <p class="font-light text-xs">{item.subtitle}</p>
           </div>
