d4df3f3d5added3151acd2cabc3ed30515e50fa6
ItsAndre548 <andre548luiz@gmail.com>
2024-10-14 06:30:47 -0300

    feat:CenteredText.tsx

--- sections/CenteredText.tsx
+++ sections/CenteredText.tsx
@@ -17,10 +17,10 @@ interface Props {
 export default function CenteredText({ text }: Props) {
   return (
     <div class="py-12 bg-primary w-full">
-      <div class="container gap-8 flex justify-center shrink-0 overflow-auto mx-auto max-w-[1440px]">
+      <div class="flex items-center overflow-auto mx-auto bg-primary w-screen px-4 md:max-w-[1440px] py-3 h-auto">
         {text &&
           text.map((item) => (
-            <div class="flex flex-col w-[90%] md:w-1/4 md:px-8 overflow-auto text-center justify-center text-primary-content">
+            <div class="flex flex-col md:px-8 overflow-auto flex-shrink-0 w-[80%] md:w-1/3 px-8 text-center justify-center text-primary-content">
               <p class="text-lg font-bold">{item.mainTitle}</p>
               <div
                 class="font-light"
@@ -32,3 +32,5 @@ export default function CenteredText({ text }: Props) {
     </div>
   );
 }
+
+/**flex items-center overflow-auto mx-auto bg-primary  px-4 md:max-w-[1440px] py-3 h-auto */
