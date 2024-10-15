28e5c4cd9c5e816f86cee2f299f170da6e6a8c13
Karla Oshikawa <karla_hara@hotmail.com>
2024-10-16 07:44:27 +0900

    feat: search page

---
+++ components/product/ProductCardSearch.tsx
@@ -0,0 +1,233 @@
+import type { Product } from "apps/commerce/types.ts";
+import { mapProductToAnalyticsItem } from "apps/commerce/utils/productToAnalyticsItem.ts";
+import Image from "apps/website/components/Image.tsx";
+import { clx } from "../../sdk/clx.ts";
+import { formatPrice } from "../../sdk/format.ts";
+import { relative } from "../../sdk/url.ts";
+import { useOffer } from "../../sdk/useOffer.ts";
+import { useSendEvent } from "../../sdk/useSendEvent.ts";
+import { useVariantPossibilities } from "../../sdk/useVariantPossiblities.ts";
+import WishlistButton from "../wishlist/WishlistButton.tsx";
+import AddToCartButton from "./AddToCartButton.tsx";
+import { Ring } from "./ProductVariantSelector.tsx";
+import { useId } from "../../sdk/useId.ts";
+
+interface Props {
+  product: Product;
+  /** Preload card image */
+  preload?: boolean;
+
+  /** @description used for analytics event */
+  itemListName?: string;
+
+  /** @description index of the product card in the list */
+  index?: number;
+
+  class?: string;
+}
+
+const WIDTH = 295;
+const HEIGHT = 378;
+const ASPECT_RATIO = `${WIDTH} / ${HEIGHT}`;
+
+function ProductCard({
+  product,
+  preload,
+  itemListName,
+  index,
+  class: _class,
+}: Props) {
+  const id = useId();
+
+  const { url, image: images, offers, isVariantOf } = product;
+  const hasVariant = isVariantOf?.hasVariant ?? [];
+  const title = isVariantOf?.name ?? product.name;
+  const [front, back] = images ?? [];
+
+  const { listPrice, price, seller = "1", availability } = useOffer(offers);
+  const inStock = availability === "https://schema.org/InStock";
+  const possibilities = useVariantPossibilities(hasVariant, product);
+  const firstSkuVariations = Object.entries(possibilities)?.[0];
+  const variants = Object.entries(firstSkuVariations?.[1] ?? {});
+  const relativeUrl = relative(url);
+  const percent = listPrice && price
+    ? Math.round(((listPrice - price) / listPrice) * 100)
+    : 0;
+
+  const item = mapProductToAnalyticsItem({ product, price, listPrice, index });
+
+  {/* Add click event to dataLayer */}
+  const event = useSendEvent({
+    on: "click",
+    event: {
+      name: "select_item" as const,
+      params: {
+        item_list_name: itemListName,
+        items: [item],
+      },
+    },
+  });
+
+  //Added it to check the variant name in the SKU Selector later, so it doesn't render the SKU to "shoes size" in the Product Card
+  const firstVariantName = firstSkuVariations?.[0]?.toLowerCase();
+  const shoeSizeVariant = "shoe size";
+
+  return (
+    <div
+      {...event}
+      class={clx(
+        "card card-compact group text-sm min-w-[295px] min-h-[498px]",
+        _class
+      )}
+    >
+      <figure
+        class={clx(
+          "relative bg-base-100  min-w-[295px] min-h-[498px]",
+          "rounded border border-transparent",
+          "group-hover:border-primary"
+        )}
+        style={{ aspectRatio: ASPECT_RATIO }}
+      >
+        {/* Product Images */}
+        <a
+          href={relativeUrl}
+          aria-label="view product"
+          class={clx(
+            "absolute top-0 left-0",
+            "grid grid-cols-1 grid-rows-1",
+            "w-full",
+            !inStock && "opacity-70"
+          )}
+        >
+          <Image
+            src={front.url!}
+            alt={front.alternateName}
+            width={295}
+            height={378}
+            style={{ aspectRatio: ASPECT_RATIO }}
+            class={clx(
+              "object-contain",
+              "rounded w-full",
+              "col-span-full row-span-full"
+            )}
+            preload={preload}
+            loading={preload ? "eager" : "lazy"}
+            decoding="async"
+          />
+          <Image
+            src={back?.url ?? front.url!}
+            alt={back?.alternateName ?? front.alternateName}
+            width={295}
+            height={378}
+            style={{ aspectRatio: ASPECT_RATIO }}
+            class={clx(
+              "object-contain",
+              "rounded w-full",
+              "col-span-full row-span-full",
+              "transition-opacity opacity-0 lg:group-hover:opacity-100"
+            )}
+            loading="lazy"
+            decoding="async"
+          />
+        </a>
+
+        {/* Wishlist button */}
+        {/* <div class="absolute top-0 left-0 w-full flex items-center justify-between">
+
+          <span
+            class={clx(
+              "text-sm/4 font-normal text-black bg-error bg-opacity-15 text-center rounded-badge px-2 py-1",
+              inStock && "opacity-0"
+            )}
+          >
+            Notify me
+          </span>
+          </div> */}
+
+        {/* Discounts */}
+        <span
+          class={clx(
+            "absolute top-2 left-2",
+            "text-[12px] font-normal text-base-100  bg-primary  text-center rounded-[4px] px-2 py-1",
+            (percent < 1 || !inStock) && "opacity-0"
+          )}
+        >
+          {percent} % off
+        </span>
+
+        {/* <div class="absolute bottom-0 right-0">
+          <WishlistButton item={item} variant="icon" />
+        </div> */}
+      </figure>
+
+      <a href={relativeUrl} class="pt-4">
+        <span class="font-medium text-sm">{title}</span>
+
+        <div class="flex gap-2 pt-2">
+          {listPrice && (
+            <span class="line-through font-normal text-gray-600 text-[10px]">
+              {formatPrice(listPrice, offers?.priceCurrency)}
+            </span>
+          )}
+          <span class="font-semibold text-secondary text-[12px]">
+            {formatPrice(price, offers?.priceCurrency)}
+          </span>
+        </div>
+      </a>
+
+      {/* SKU Selector */}
+      {/* {variants.length > 1 && firstVariantName !== shoeSizeVariant && (
+        <ul class="flex items-center justify-start gap-2 pt-4 pb-1 pl-1 overflow-x-auto">
+          {variants
+            .map(([value, link]) => [value, relative(link)] as const)
+            .map(([value, link]) => (
+              <li>
+                <a href={link} class="cursor-pointer">
+                  <input
+                    class="hidden peer"
+                    type="radio"
+                    name={`${id}-${firstSkuVariations?.[0]}`}
+                    checked={link === relativeUrl}
+                  />
+                  <Ring value={value} checked={link === relativeUrl} />
+                </a>
+              </li>
+            ))}
+        </ul>
+      )} */}
+
+      <div class="flex-grow" />
+
+      <div>
+        {inStock ? (
+          <AddToCartButton
+            product={product}
+            seller={seller}
+            item={item}
+            class={clx(
+              "btn",
+              "btn-outline justify-center border-gray-300 !text-sm !font-medium px-0 no-animation w-full",
+              "hover:!bg-primary",
+              "hover:!text-base-100"
+            )}
+          />
+        ) : (
+          <a
+            href={relativeUrl}
+            class={clx(
+              "btn",
+              "btn-outline justify-start border-none !text-sm !font-medium px-0 no-animation w-full h-29",
+              "hover:!bg-transparent",
+              "disabled:!bg-transparent disabled:!opacity-75",
+              "btn-error hover:!text-error disabled:!text-error"
+            )}
+          >
+            Fora de estoque
+          </a>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
--- components/search/Filters.tsx
+++ components/search/Filters.tsx
@@ -9,20 +9,37 @@ import Avatar from "../../components/ui/Avatar.tsx";
 import { clx } from "../../sdk/clx.ts";
 import { formatPrice } from "../../sdk/format.ts";
 
+export interface CategoryFilter {
+  title: string;
+  links: {
+    label: string;
+    url: string;
+  }[];
+}
+
 interface Props {
   filters: ProductListingPage["filters"];
+  categoryList?: CategoryFilter;
 }
 
 const isToggle = (filter: Filter): filter is FilterToggle =>
   filter["@type"] === "FilterToggle";
 
-function ValueItem(
-  { url, selected, label, quantity }: FilterToggleValue,
-) {
+function ValueItem({ url, selected, label, quantity }: FilterToggleValue) {
+  const startsWithLetter = /^[a-zA-Z]/.test(label);
   return (
     <a href={url} rel="nofollow" class="flex items-center gap-2">
       <div aria-checked={selected} class="checkbox" />
-      <span class="text-sm">{label}</span>
+      <span class="text-sm first-letter:uppercase">
+        {startsWithLetter
+          ? label.replace("-", " ")
+          : label
+              .split("_")
+              .map(
+                (parte) => `R$${parseFloat(parte).toFixed(2).replace(".", ",")}`
+              )
+              .join(" ~ ")}
+      </span>
       {quantity > 0 && <span class="text-sm text-base-400">({quantity})</span>}
     </a>
   );
@@ -33,7 +50,7 @@ function FilterValues({ key, values }: FilterToggle) {
   const flexDirection = avatars ? "flex-row items-center" : "flex-col";
 
   return (
-    <ul class={clx(`flex flex-wrap gap-2`, flexDirection)}>
+    <ul class={clx(`flex flex-wrap gap-2 `, flexDirection)}>
       {values.map((item) => {
         const { url, selected, value } = item;
 
@@ -48,15 +65,10 @@ function FilterValues({ key, values }: FilterToggle) {
           );
         }
 
-        if (key === "price") {
+        if (key === "price-range") {
           const range = parseRange(item.value);
 
-          return range && (
-            <ValueItem
-              {...item}
-              label={`${formatPrice(range.from)} - ${formatPrice(range.to)}`}
-            />
-          );
+          return range && <ValueItem {...item} />;
         }
 
         return <ValueItem {...item} />;
@@ -65,18 +77,88 @@ function FilterValues({ key, values }: FilterToggle) {
   );
 }
 
-function Filters({ filters }: Props) {
+function Filters({ filters, categoryList }: Props) {
   return (
-    <ul class="flex flex-col gap-6 p-4 sm:p-0">
-      {filters
-        .filter(isToggle)
-        .map((filter) => (
-          <li class="flex flex-col gap-4">
-            <span>{filter.label}</span>
-            <FilterValues {...filter} />
-          </li>
-        ))}
-    </ul>
+    <div class="flex gap-3">
+      <div class="relative">
+        <input type="checkbox" id="category-toggle" class="peer hidden" />
+
+        <label
+          for="category-toggle"
+          class="select w-[193px] h-6 rounded-md border border-secondary  min-h-0 text-[10px]"
+        >
+          Categoria
+        </label>
+
+        <div class="absolute top-12 bg-white shadow-lg p-4 rounded-lg transition-all duration-300 opacity-0 invisible peer-checked:opacity-100 peer-checked:visible">
+          <ul class="flex flex-col gap-6 p-4 sm:p-0 mt-6">
+            {filters.filter(isToggle).map(
+              (filter) =>
+                filter.label === "categoria" && (
+                  <li key={filter.label} class="flex flex-col gap-4">
+                    <span class="text-sm font-semibold first-letter:uppercase">
+                      {filter.label}
+                    </span>
+                    <FilterValues {...filter} />
+                  </li>
+                )
+            )}
+          </ul>
+        </div>
+      </div>
+      <div class="relative">
+        <input type="checkbox" id="subcategory-toggle" class="peer hidden" />
+
+        <label
+          for="subcategory-toggle"
+          class="select w-[193px] h-6 rounded-md border border-secondary  min-h-0 text-[10px]"
+        >
+          Subcategoria
+        </label>
+
+        <div class="absolute top-12 bg-white shadow-lg p-4 rounded-lg transition-all duration-300 opacity-0 invisible peer-checked:opacity-100 peer-checked:visible">
+          <ul class="flex flex-col gap-6 p-4 sm:p-0 mt-6">
+            {filters.filter(isToggle).map(
+              (filter) =>
+                filter.label === "subcategoria" && (
+                  <li key={filter.label} class="flex flex-col gap-4">
+                    <span class="text-sm font-semibold first-letter:uppercase">
+                      {filter.label}
+                    </span>
+                    <FilterValues {...filter} />
+                  </li>
+                )
+            )}
+          </ul>
+        </div>
+      </div>
+      <div class="relative">
+        <input type="checkbox" id="price-toggle" class="peer hidden" />
+
+        <label
+          for="price-toggle"
+          class="select w-[193px] h-6 rounded-md border border-secondary  min-h-0 text-[10px]"
+        >
+          Preço
+        </label>
+
+        <div class="absolute top-12 bg-white shadow-lg p-4 rounded-lg transition-all duration-300 opacity-0 invisible peer-checked:opacity-100 peer-checked:visible">
+          <ul class="flex flex-col gap-6 p-4 sm:p-0 mt-6">
+            {filters.filter(isToggle).map(
+              (filter) =>
+                filter.label === "filtro-preco" && (
+                  <li key={filter.label} class="flex flex-col gap-4">
+                    <span class="text-sm font-semibold first-letter:uppercase">
+                      {filter.label}
+                    </span>
+                    <FilterValues {...filter} />
+                  </li>
+                )
+            )}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
 
