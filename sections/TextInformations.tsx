4a966f3a05a95ec1f5f616ed6338b2b92fb1df1c
Karla Oshikawa <karla_hara@hotmail.com>
2024-11-07 01:30:29 +0900

    feat: text information styles

--- sections/TextInformations.tsx
+++ sections/TextInformations.tsx
@@ -10,7 +10,7 @@ export default function Section({ title, conteudo }: Props) {
     <div class="max-w-[1440px] mx-auto mt-16 ">
       <h2 class="text-2xl text-primary text-center">{title ? title : ""}</h2>
       <div
-        class="px-10 mt-12 text-sm leading-6"
+        class="px-10 mt-12 reset_styles"
         dangerouslySetInnerHTML={{ __html: conteudo ? conteudo : "" }}
       />
     </div>
