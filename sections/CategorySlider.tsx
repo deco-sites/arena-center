481389a7184ac93c90236f407780e004d03a6673
Karla Oshikawa <karla_hara@hotmail.com>
2024-11-20 02:38:52 +0900

    feat: category slyder

--- sections/CategorySlider.tsx
+++ sections/CategorySlider.tsx
@@ -31,7 +31,7 @@ function CategorySlider({ categoryItems, itemListName }: Props) {
           gridTemplateColumns: "min-content 1fr min-content",
         }}
       >
-        <div class="col-span-3 col-start-1 row-span-1 mx-auto row-start-1 px-6 md:px-0 max-w-[1300px]">
+        <div class="col-span-3 col-start-1 row-span-1 mx-auto row-start-1 px-6 md:px-0 max-w-[1200px]">
           <Slider class="carousel carousel-center sm:carousel-end gap-2 w-full">
             {categoryItems?.map((item, index) => (
               <Slider.Item
@@ -39,7 +39,7 @@ function CategorySlider({ categoryItems, itemListName }: Props) {
                 class={clx(
                   "carousel-item",
                   "first:pl-0 first:sm:pl-0",
-                  "last:pr-0 last:sm:pr-0",
+                  "last:pr-0 last:sm:pr-0"
                 )}
               >
                 <div class="bg-secondary-content">
@@ -48,9 +48,9 @@ function CategorySlider({ categoryItems, itemListName }: Props) {
                       src={item.image}
                       width={295}
                       height={305}
-                      class="w-[287px] sm:w-[300px]"
+                      class="w-[287px] sm:w-[295px] h-auto"
                     />
-                    <p class="pt-[5px] text-xs pb-[7px] pl-[9px]">
+                    <p class="pt-[5px] text-xs pb-[7px] pl-[9px] h-[29px]">
                       {item.paragraph}
                     </p>
                   </a>
@@ -60,13 +60,13 @@ function CategorySlider({ categoryItems, itemListName }: Props) {
           </Slider>
         </div>
         <div class="relative bottom-[7%] z-10 col-span-1 col-start-1 row-span-1 row-start-1 p-2 self-center">
-          <Slider.PrevButton class="sm:flex hidden disabled:invisible no-animation">
+          <Slider.PrevButton class="sm:flex hidden disabled:opacity-0 no-animation">
             <Icon id="chevron-right" class="rotate-180 text-accent-content" />
           </Slider.PrevButton>
         </div>
 
         <div class="relative bottom-[7%] z-10 col-span-1 col-start-3 row-span-1 row-start-1 p-2 self-center">
-          <Slider.NextButton class="sm:flex hidden disabled:invisible no-animation">
+          <Slider.NextButton class="sm:flex hidden disabled:opacity-50 no-animation">
             <Icon id="chevron-right" class="text-accent-content" />
           </Slider.NextButton>
         </div>
