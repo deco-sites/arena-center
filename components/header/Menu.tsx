623c1f017103e73b3c5324a9c13795f5d1a3d3f1
Karla Oshikawa <karla_hara@hotmail.com>
2024-12-19 08:15:24 +0900

    feat: menu mobile

--- components/header/Menu.tsx
+++ components/header/Menu.tsx
@@ -50,7 +50,7 @@ function Menu({ navItems = [] }: Props) {
       class="flex flex-col h-full overflow-y-auto"
       style={{ minWidth: "100vw" }}
     >
-      <ul class="px-4 flex-grow flex flex-col divide-y divide-base-200 overflow-y-auto">
+      <ul class="px-4 flex-grow flex flex-col divide-y divide-base-200 overflow-y-auto text-accent-content">
         {navItems.map((item) => (
           <li>
             <MenuItem item={item} />
