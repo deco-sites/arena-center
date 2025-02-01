a43161be9d5775784a204704c6548338df2ff4b5
Karla Oshikawa <karla_hara@hotmail.com>
2025-02-01 23:39:51 +0900

    feat: mobile ajust

--- components/product/BuyTogetherComponent.tsx
+++ components/product/BuyTogetherComponent.tsx
@@ -61,7 +61,29 @@ export default function BuyTogetherComponent({ page, products, icons }: Props) {
     <div class="container flex flex-col gap-2 sm:gap-5 md:w-full pt-8 items-center py-5 w-[350px] mt-2 mx-auto">
       {page !== undefined && secondProduct !== undefined && (
         <div class="flex flex-col ">
-          <div class="flex md:flex-row flex-col md:items-center mx-auto md:gap-1">
+          {/* Shipping Simulation */}
+          <div class="">
+            <ShippingSimulationForm
+              items={[{ id: Number(product.sku), quantity: 1, seller: seller }]}
+            />
+          </div>
+
+          {/* Garanty Icons */}
+          <div class="flex w-full px-11 justify-between pt-6 border-t border-gray-300">
+            {icons &&
+              icons.map((item) => (
+                <div class="flex items-center flex-col gap-3 ">
+                  <Image
+                    src={item.icon}
+                    width={31}
+                    height={31}
+                    alt={item.title}
+                  />
+                  <p class="text-accent-content text-[12px]">{item.title}</p>
+                </div>
+              ))}
+          </div>
+          <div class="flex md:flex-row flex-col md:items-center mx-auto md:gap-1 pt-10">
             <div class="flex items-center">
               <ProductCardBuyTogether
                 index={1}
@@ -97,47 +119,27 @@ export default function BuyTogetherComponent({ page, products, icons }: Props) {
                 </div>
               </div>
 
-              {inStock && secondProduct
-                ? (
-                  <AddToCartBuyTogether
-                    products={[product, secondProduct]}
-                    class={clx("")}
-                    icon="https://deco-sites-assets.s3.sa-east-1.amazonaws.com/arena-center/8ffcddc7-e6c9-4326-81f2-79c0abbee7a0/CART_02.svg"
-                  />
-                )
-                : (
-                  <p
-                    class={clx(
-                      "btn",
-                      "btn-outline justify-center  !text-[12px] !font-medium px-0 no-animation w-full",
-                      "text-center border border-secondary btn-secondary min-h-0 h-[26px]",
-                    )}
-                  >
-                    Fora de estoque
-                  </p>
-                )}
+              {inStock && secondProduct ? (
+                <AddToCartBuyTogether
+                  products={[product, secondProduct]}
+                  class={clx("")}
+                  icon="https://deco-sites-assets.s3.sa-east-1.amazonaws.com/arena-center/8ffcddc7-e6c9-4326-81f2-79c0abbee7a0/CART_02.svg"
+                />
+              ) : (
+                <p
+                  class={clx(
+                    "btn",
+                    "btn-outline justify-center  !text-[12px] !font-medium px-0 no-animation w-full",
+                    "text-center border border-secondary btn-secondary min-h-0 h-[26px]"
+                  )}
+                >
+                  Fora de estoque
+                </p>
+              )}
             </div>
           </div>
         </div>
       )}
-
-      {/* Shipping Simulation */}
-      <div class="">
-        <ShippingSimulationForm
-          items={[{ id: Number(product.sku), quantity: 1, seller: seller }]}
-        />
-      </div>
-
-      {/* Garanty Icons */}
-      <div class="flex w-full px-11 justify-between pt-6 border-t border-gray-300">
-        {icons &&
-          icons.map((item) => (
-            <div class="flex items-center flex-col gap-3 ">
-              <Image src={item.icon} width={31} height={31} alt={item.title} />
-              <p class="text-accent-content text-[12px]">{item.title}</p>
-            </div>
-          ))}
-      </div>
     </div>
   );
 }
