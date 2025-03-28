cc38431888e39afdc1ff0d4b4c3ca07cfe55d420
gabrielMatosBoubee <gabriel.boubee@carcara.tech>
2025-03-28 17:39:53 -0300

    fix: Add fallback for collapse in menu

--- components/header/Menu.tsx
+++ components/header/Menu.tsx
@@ -1,41 +1,58 @@
-import Icon from "../../components/ui/Icon.tsx";
 import type { SiteNavigationElement } from "apps/commerce/types.ts";
+import { useScript } from "@deco/deco/hooks";
+import { useId } from "site/sdk/useId.ts";
 
 export interface Props {
   navItems?: SiteNavigationElement[];
 }
 
 function MenuItem({ item }: { item: SiteNavigationElement }) {
+  function toogleOpen(id: string) {
+    const collapse = document.getElementById(id) as HTMLDivElement;
+    collapse.classList.toggle("collapse-open");
+  }
+  const id = useId();
   return (
-    <div class="collapse collapse-plus">
-      <input type="checkbox" class="min-h-[0]" />
+    <div class="collapse collapse-plus" id={id}>
+      <input
+        hx-on:change={useScript(toogleOpen, id)}
+        type="checkbox"
+        class="w-12 ml-auto"
+      />
       <a class="collapse-title capitalize text-left" href={item.url}>
         {item.name}
       </a>
       <div class="collapse-content">
         <ul>
-          {item.children?.map((node) => (
-            <li>
-              <div class="collapse collapse-plus">
-                <input type="checkbox" />
-                <a
-                  class="collapse-title capitalize font-medium"
-                  href={node.url}
-                >
-                  {node.name}
-                </a>
-                <div class="collapse-content">
-                  <ul>
-                    {item.children?.map((nodeItem) => (
-                      <li class="mt-4">
-                        <a href={nodeItem.url}>{nodeItem.name}</a>
-                      </li>
-                    ))}
-                  </ul>
+          {item.children?.map((node) => {
+            const childrenId = useId();
+            return (
+              <li>
+                <div class="collapse collapse-plus" id={childrenId}>
+                  <input
+                    hx-on:change={useScript(toogleOpen, childrenId)}
+                    type="checkbox"
+                    class="w-12 ml-auto"
+                  />
+                  <a
+                    class="collapse-title capitalize font-medium"
+                    href={node.url}
+                  >
+                    {node.name}
+                  </a>
+                  <div class="collapse-content">
+                    <ul>
+                      {item.children?.map((nodeItem) => (
+                        <li class="mt-4">
+                          <a href={nodeItem.url}>{nodeItem.name}</a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
