830bc6826a6ca178b4417f57b99ec5cdd3884298
gabrielMatosBoubee <gabriel.boubee@carcara.tech>
2025-04-28 17:51:09 -0300

    fix: gap

--- sections/ThreeImages.tsx
+++ sections/ThreeImages.tsx
@@ -18,7 +18,7 @@ export default function ({ img }: Props) {
   const device = useDevice();
   if (device !== "desktop") {
     return (
-      <div class="flex gap-3 pl-6 overflow-auto max-w-[1440px] w-full mx-auto mt-7 custom-container">
+      <div class="flex gap-2 pl-6 overflow-auto max-w-[1440px] w-full mx-auto mt-7 custom-container">
         {img.map((item) => (
           <div class="relative">
             <a href={item.link} class="w-[80vw] block lg:w-auto">
